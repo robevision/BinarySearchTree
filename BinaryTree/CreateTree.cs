2588ac7653f6f9c265d04e44d519c2e3ab625949
Robe Gogan <robe.gogan@gmail.comgit config --global user.email robe.gogan@gmail.comgit push origin master:qahthing>
2019-02-15 07:43:03 -0600

    Linked repository and generated Node class and CreateTree class.

---
+++ BinaryTree/CreateTree.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTree
+{
+    class CreateTree
+    {
+        Node root;
+        public  CreateTree()
+        {
+            root = new Node(4);
+        }
+    
+        //wrap around array
+    }
+}
---
+++ BinaryTree/Node.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTree
+{
+    public class Node
+    {
+        public int data;
+        Node left;
+        Node right;
+        public Node link;
+        public Node(int data)
+        {
+            this.data = data;
+            link = null;
+            left = null;
+            right = null;
+        }
+    }
+}
